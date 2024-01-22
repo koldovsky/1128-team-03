092cdc4121e6aef8cf050574974c24d05096d696
PyTrainee <eternal@disroot.org>
2024-01-22 17:09:02 +0200

    js

--- codewars/team_best.js
+++ codewars/team_best.js
@@ -1,11 +1,11 @@
 /*
-
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 //1 Messi Goals
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     const totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
     return totalGoals;
 }
-
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 //2 Return Negative
 function makeNegative(num) {
     if (num > 0){
@@ -13,13 +13,13 @@ function makeNegative(num) {
     }
     return num;
 }
-
+// https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 //3 Game Move
 function move (position, roll) {
     const newPosition = position + roll * 2;
     return newPosition;
 }
-
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 //4 Grasshopper - Personalized Message
 function greet (name, owner) {
     if (owner === name) {
@@ -28,12 +28,12 @@ function greet (name, owner) {
         return ('Hello guest');
     }
 }
-
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 //5 Keep Hydrated
 function litres(time) {
     return Math.floor(time * 0.5);
 }
-
+// https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 //6 Opposites Attract
 function lovefunc(flower1, flower2){
     return (
