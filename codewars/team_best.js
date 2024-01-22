b6699fcc9140450e20d6c28e59fe8239eeb440d2
PyTrainee <eternal@disroot.org>
2024-01-22 15:51:47 +0200

    mob media

--- codewars/team_best.js
+++ codewars/team_best.js
@@ -1,10 +1,12 @@
-//https://www.codewars.com/users/2024JS
-//Grasshopper - Messi goals function
+/*
+
+//1 Messi Goals
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-    return(laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+    const totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    return totalGoals;
 }
 
-//Return Negative
+//2 Return Negative
 function makeNegative(num) {
     if (num > 0){
         num = num * -1;
@@ -12,52 +14,29 @@ function makeNegative(num) {
     return num;
 }
 
-//Grasshopper - Terminal game move function
+//3 Game Move
 function move (position, roll) {
-    return (position + roll*2)
+    const newPosition = position + roll * 2;
+    return newPosition;
 }
 
-//Grasshopper - Personalized Message
+//4 Grasshopper - Personalized Message
 function greet (name, owner) {
-    if (owner == name) {
+    if (owner === name) {
         return ('Hello boss');
     } else {
         return ('Hello guest');
     }
 }
 
-//Keep Hydrated!
+//5 Keep Hydrated
 function litres(time) {
     return Math.floor(time * 0.5);
 }
 
-//Opposites Attract
+//6 Opposites Attract
 function lovefunc(flower1, flower2){
     return (
         (flower1 + flower2) % 2 === 0 ? false : true)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
