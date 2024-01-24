42d1ace71017ea600d2e096f880509f0f19bbba7
PyTrainee <eternal@disroot.org>
2024-01-24 17:35:24 +0200

    js2-5

--- codewars/2-quest.js
+++ codewars/2-quest.js
@@ -1,18 +1,12 @@
 //1 https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
-    return (string.split(' '))
-}
+const stringToArray = (string) => string.split(' ');
 
 //2 https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
-function DNAtoRNA(dna) {
-    return dna.replace(/T/g, 'U')
-}
+const DNAtoRNA = (dna) => dna.replace(/T/g, 'U');
 
 //3 https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 const min = (list) => Math.min(...list);
 const max = (list) => Math.max(...list);
 
 //4 https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
-function min(arr, toReturn) {
-    return (toReturn === "value" ? Math.min(...arr) : arr.indexOf(Math.min(...arr)))
-}
+const min = (arr, toReturn) => (toReturn === "value" ? Math.min(...arr) : arr.indexOf(Math.min(...arr)));
