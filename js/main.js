e9b9ed88be3209b4d4d2ea5f6be89cd186dda49e
Aleksandr Koniev <0719118@gmail.com>
2024-01-26 14:09:23 +0000

    Fix button UP

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-const btn = document.querySelector('.btn-scroll-up');
-
-const handleScroll = () => {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (scrollPosition > 100) {
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('.btn-scroll-up');
+  
+    const handleScroll = () => {
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+      if (scrollPosition > 100) {
         btn.classList.add('show');
-    } else {
+      } else {
         btn.classList.remove('show');
-    }
-};
-
-const handlerScrollUp = () => {
-    window.scrollTo({
+      }
+    };
+  
+    const handlerScrollUp = () => {
+      window.scrollTo({
         top: 0,
         left: 0,
         behavior: 'smooth',
-    });
-};
-
-window.addEventListener('scroll', handleScroll);
-
-btn.addEventListener('click', handlerScrollUp);
+      });
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+  
+    btn.addEventListener('click', handlerScrollUp);
+  });
+  
