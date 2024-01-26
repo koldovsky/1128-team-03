9ed96e3d7e35efdcfa23151029abb65b6fbf8ea1
Aleksandr Koniev <0719118@gmail.com>
2024-01-26 13:32:32 +0200

    Update 2-quest.js

--- codewars/2-quest.js
+++ codewars/2-quest.js
@@ -76,3 +76,17 @@ function min(arr, toReturn) {
     return arr.indexOf(minNumber(arr));
   }
 }
+
+//5 https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+//Koniev Alex
+function warnTheSheep(queue) {
+ const wolfIndex = queue.indexOf("wolf");
+    
+    if (wolfIndex === queue.length - 1) {
+        return "Pls go away and stop eating my sheep";
+    } else {
+        const sheepNumber = queue.length - 1 - wolfIndex;
+        return `Oi! Sheep number ${sheepNumber}! You are about to be eaten by a wolf!`;
+    }
+}
