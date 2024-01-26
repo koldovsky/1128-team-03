9dc40e643844fa408bea085d71486bd631154cbb
Aleksandr Koniev <0719118@gmail.com>
2024-01-26 18:04:15 +0000

    Fix JS

--- js/footer.js
+++ js/footer.js
@@ -1,26 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.querySelector('.btn-scroll-up');
-  
-    const handleScroll = () => {
-      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  
-      if (scrollPosition > 100) {
-        btn.classList.add('show');
-      } else {
-        btn.classList.remove('show');
-      }
-    };
-  
-    const handlerScrollUp = () => {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-      });
-    };
-  
-    window.addEventListener('scroll', handleScroll);
-  
-    btn.addEventListener('click', handlerScrollUp);
-  });
-  
+
+  const btn = document.querySelector('.btn-scroll-up');
+
+  const handleScroll = () => {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollPosition > 100) {
+      btn.classList.add('show');
+    } else {
+      btn.classList.remove('show');
+    }
+  };
+
+  const handlerScrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  btn.addEventListener('click', handlerScrollUp);
