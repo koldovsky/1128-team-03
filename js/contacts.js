85cb7fc386bcd5d76181e94cdc769beb049ed580
Yulii-Example <152956627+Yulii-Example@users.noreply.github.com>
2024-01-27 13:43:00 +0000

    Add time

--- js/contacts.js
+++ js/contacts.js
@@ -1,2 +1,16 @@
-const contactsTitle = document.querySelector("contacts__title");
-contactsTitle.innerText = "First Step";
+const contactsTitle = document.querySelector('.contacts__title');
+contactsTitle.innerText = 'First Step!';
+
+const greetings = [
+      'Hello',
+      'Olla',
+      'Hey',
+      'Bro',
+];
+
+contactsTitle.innerText = getRandomEl(greetings);
+function getRandomEl(arr) {
+    return arr[ Math.floor(Math.random() * arr.length)];
+}
+const clockContainer = document.querySelector('.contacts_clock')
+clockContainer.innerText = new Date().toLocaleTimeString("uk")
