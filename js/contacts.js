066fb0f2daaca45fee4480ed89909c5dfc60be5d
Yulii-Example <152956627+Yulii-Example@users.noreply.github.com>
2024-01-28 09:27:25 +0000

    Changed CSS

--- js/contacts.js
+++ js/contacts.js
@@ -1,17 +1,3 @@
-const contactsTitle = document.querySelector('.contacts__title');
-contactsTitle.innerText = 'First Step!';
-
-const greetings = [
-      'Hello',
-      'Olla',
-      'Hey',
-      'Yo',
-];
-
-contactsTitle.innerText = getRandomEl(greetings);
-function getRandomEl(arr) {
-    return arr[ Math.floor(Math.random() * arr.length)];
-}
 
 const clockContainer = document.querySelector('.contacts_clock');
 function updateClock() {
