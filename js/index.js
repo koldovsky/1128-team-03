752516650b5ef4cffd0f0ce82105b46f969cad8e
Zaikov123 <zaikovarseniy@gmail.com>
2024-01-31 01:39:58 +0200

    removed the clone folder

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-document.addEventListener('partialsLoaded', () => {
-    import('./footer.js');
-    import('./contacts.js');
-    import('./header.js');
-    import('./cat-surprise.js');
-    import('./workers-counter.js');
-
+document.addEventListener("partialsLoaded", () => {
+  import("./footer.js");
+  import("./contacts.js");
+  import("./header.js");
+  import("./cat-surprise.js");
+  import("./workers-counter.js");
+  import("./dont-click.js");
 });
 
 function soundClick() {
-    const audio = new Audio(); // Создаём новый элемент Audio
-    audio.src = "audio/great-site.ogg"; // Указываем путь к звуку "клика"
-    audio.autoplay = true; // Автоматически запускаем
+  const audio = new Audio(); // Создаём новый элемент Audio
+  audio.src = "audio/great-site.ogg"; // Указываем путь к звуку "клика"
+  audio.autoplay = true; // Автоматически запускаем
 }
