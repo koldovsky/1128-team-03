473b408da1d79f2b21df7ceb23cf8e81c7022012
PyTrainee <eternal@disroot.org>
2024-01-30 03:11:02 +0200

    DB connect, Post-to-DB

--- models/post.js
+++ models/post.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const postSchema = new Schema({
-    message: {
-        type: String,
-        required: true,
-    }
+    message:
+        {
+            type: String,
+            required: true,
+        }
 });
+
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
