c1b8d058045030134fb537e7b7942436b046a1aa
JSTrainee <54483808+2024JS@users.noreply.github.com>
2024-01-30 04:00:13 +0300

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,1 +1,10 @@
-
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    message: {
+        type: String,
+        required: true,
+    }
+});
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
