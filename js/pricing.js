7faf9abef5d0084e220d88127737428e1751c05a
Olena Kuziaieva <kusaeva@gmail.com>
2024-02-05 23:36:12 +0100

    make pricing carousel animated

--- js/pricing.js
+++ js/pricing.js
@@ -1,101 +1,41 @@
-/*const slides = [
-  `
-    <article class="pricing__carousel-slide">
-            <p class="pricing__price">$50</p>
-            <h3 class="pricing__plan-title">Initial Checkup</h3>
-            <ul class="pricing__features">
-              <li class="pricing__features-checkmark">
-                Checkup by a veterinarian
-              </li>
-              <li class="pricing__features-checkmark">Care recommendations</li>
-              <li class="pricing__features-crossmark">Laboratory tests</li>
-              <li class="pricing__features-crossmark">Dental care</li>
-            </ul>
-            <div class="pricing__select-button mt-md-3 mb-md-5">
-              <a href="#" class="pricing__select-plan "
-                ><span class="pricing__select-bn-content">Select Plan</span
-                ><span class="pricing__select-bn-arrow">&rarr;</span>
-              </a>
-            </div>
-    </article>
-    `,
-  `
-<article class="pricing__carousel-slide">
-    <p class="pricing__price">$75</p>
-    <h3 class="pricing__plan-title">Full Checkup</h3>
-    <ul class="pricing__features">
-      <li class="pricing__features-checkmark">
-        Checkup by a veterinarian
-      </li>
-      <li class="pricing__features-checkmark">Checkup by a dentist</li>
-      <li class="pricing__features-checkmark">Laboratory tests</li>
-      <li class="pricing__features-crossmark">Preventive vaccination</li>
-    </ul>
-    <div class="pricing__select-button">
-      <a href="#" class="pricing__select-plan"
-        ><span class="pricing__select-bn-content">Select Plan</span
-        ><span class="pricing__select-bn-arrow">&rarr;</span>
-      </a>
-    </div>
-  </article> 
-  `,
-  `
-    <article class="pricing__carousel-slide">
-            <p class="pricing__price">$100</p>
-            <h3 class="pricing__plan-title">Vaccination</h3>
-            <ul class="pricing__features">
-              <li class="pricing__features-checkmark">3 available features</li>
-              <li class="pricing__features-checkmark">10 requests per month</li>
-              <li class="pricing__features-checkmark">General support</li>
-              <li class="pricing__features-checkmark">Personal consultations</li>
-            </ul>
-            <div class="pricing__select-button">
-              <a href="#" class="pricing__select-plan"
-                ><span class="pricing__select-bn-content">Select Plan</span
-                ><span class="pricing__select-bn-arrow">&rarr;</span>
-              </a>
-            </div>
-    </article>
-    `
-];
+    const carouselContainer = document.querySelector(".pricing__carousel-wrapper");
+    const carouselInnerContainer = document.querySelector(".pricing__carousel-inner");
+    const buttonLeft = document.querySelector(".pricing__button-left");
+    const buttonRight = document.querySelector(".pricing__button-right");
+    const indicators = document.querySelectorAll(".pricing__indicator");
 
-let slideIdx = 0;
+    let currentSlide = 0;
 
-const slideContainer = document.querySelector(".pricing__carousel-inner");
-
-function renderSlide() {
-  slideContainer.innerHTML = '';
-
-  slideContainer.innerHTML += slides[slideIdx];
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    const secondSlideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
-    slideContainer.innerHTML += slides[secondSlideIdx];
-    if (window.matchMedia("(min-width: 992px)").matches) {
-      const thirdSlideIdx =
-        secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-      slideContainer.innerHTML += slides[thirdSlideIdx];
+    function updateCarousel() {
+      const slideWidth = carouselInnerContainer.offsetWidth;
+      const newTranslateX = -currentSlide * slideWidth;
+      carouselContainer.style.transform = `translateX(${newTranslateX}px)`;
+      updateIndicators();
     }
-  }
-}
 
-function nextSlide() {
-    slideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
-    renderSlide();
-}
+    function updateIndicators() {
+      indicators.forEach((indicator, index) => {
+        indicator.checked = index === currentSlide;
+      });
+    }
 
-function prevSlide() {
-    slideIdx = slideIdx - 1 < 0 ? slides.length - 1 : slideIdx - 1;
-    renderSlide();
-}
+    function nextSlide() {
+      currentSlide = (currentSlide + 1) % 3;
+      updateCarousel();
+    }
 
-setInterval(nextSlide, 3000);
+    function prevSlide() {
+      currentSlide = (currentSlide - 1 + 3) % 3;
+      updateCarousel();
+    }
 
-const btnNext = document.querySelector(".pricing__button-right");
-btnNext.addEventListener("click", nextSlide);
+    buttonRight.addEventListener("click", nextSlide);
+    buttonLeft.addEventListener("click", prevSlide);
 
-const btnPrev = document.querySelector(".pricing__button-left");
-btnPrev.addEventListener("click", prevSlide);
-
-renderSlide();
-
-window.addEventListener("resize", renderSlide);*/
+    // Optional: Add indicator click event listeners
+    indicators.forEach((indicator, index) => {
+      indicator.addEventListener("click", () => {
+        currentSlide = index;
+        updateCarousel();
+      });
+    });
