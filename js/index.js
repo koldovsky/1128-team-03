0c08624c09ccc933e778f8023400f1c33014ab03
Zaikov123 <zaikovarseniy@gmail.com>
2024-02-06 02:39:58 +0200

    someone deleted my js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-document.addEventListener('partialsLoaded', () => {
-    import('./footer.js');
-    import('./contacts.js');
-    import('./header.js');
-    import('./cat-surprise.js');
-    import('./workers-counter.js');
-    import('./pricing.js');
-    import('./faq.js');
-    import('./partners-carousel.js');
-    import('./navigation.js');      
+document.addEventListener("partialsLoaded", () => {
+  import("./footer.js");
+  import("./contacts.js");
+  import("./header.js");
+  import("./cat-surprise.js");
+  import("./dont-click.js");
+  import("./workers-counter.js");
+  import("./pricing.js");
+  import("./faq.js");
+  import("./partners-carousel.js");
+  import("./navigation.js");
 });
-
-
-
