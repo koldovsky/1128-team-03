b9ce0ffcf28f911920ffc3286cb98aa87fc15500
Max <153082122+Max-Dtk@users.noreply.github.com>
2024-02-08 00:30:20 +0200

    Delete js/care.js

--- js/care.js
+++
@@ -1,7 +0,0 @@
-function changeBackground() {
-            document.querySelector('.care').style.backgroundImage = 'url("../IMG_Space_Back.JPG")';
-        }
-
-function resetBackground() {
-            document.querySelector('.care').style.backgroundImage = 'url("../IMG_CARE.JPG")';
-        }
