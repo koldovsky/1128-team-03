47867e14c172be54f79cadac3692cc4a185e8333
Max <153082122+Max-Dtk@users.noreply.github.com>
2024-02-08 00:14:27 +0200

    Create care.js

---
+++ js/care.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+            document.querySelector('.care').style.backgroundImage = 'url("../IMG_Space_Back.JPG")';
+        }
+
+function resetBackground() {
+            document.querySelector('.care').style.backgroundImage = 'url("../IMG_CARE.JPG")';
+        }
